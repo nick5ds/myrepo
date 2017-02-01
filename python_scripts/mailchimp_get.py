5cebb39ab09bc85a3fc0acc516763eb4a1b34f51
Nikhil S <nsabharwal1188@gmail.com>
2017-02-01 11:51:20 -0800

    Update mailchimp_get.py

--- python_scripts/mailchimp_get.py
+++ python_scripts/mailchimp_get.py
@@ -3,7 +3,7 @@ import json
 from pprint import pprint
 import csv
 #url='https://us9.api.mailchimp.com/3.0/reports/b3239a9fcc/sent-to'
-auth=('anystring','57902f215f2ab12b541a1cb2ae9f725c-us9')
+auth=('anystring','')
 #payload={'fields':'members.id,members.email_address,members.status','count':10}
 payload={'count':10}
 #payload={'count':100}
