bf2170d4d653e4d0787e666c8c495918ecb3aca9
Nikhil S <nsabharwal1188@gmail.com>
2017-02-01 11:49:28 -0800

    Update bg_nbd_model.py

--- python_scripts/bg_nbd_model.py
+++ python_scripts/bg_nbd_model.py
@@ -12,10 +12,10 @@ import psycopg2
 import decimal
 
 def generate_ndb_data(end_date,interval):
-    FIVETRAN_HOST ='eatsa-warehouse-fivetran.c5xyqeaknls5.us-east-1.redshift.amazonaws.com'
-    FIVETRAN_USER = 'awsuser'
-    FIVETRAN_PASSWORD ='CxoqbgkkqtWXILseCR0qeGWn3s72iFDPML8T8rMwFbpjyzfraOBBxtihPkDZq6C3'
-    FIVETRAN_DATABASE = 'fivetran'
+    FIVETRAN_HOST =
+    FIVETRAN_USER = 
+    FIVETRAN_PASSWORD =
+    FIVETRAN_DATABASE = 
     conn = psycopg2.connect(
         dbname=FIVETRAN_DATABASE,
         host=FIVETRAN_HOST,
